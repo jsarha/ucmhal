650f4da0cd445bba8b168519ae98875685096baa
Jyri Sarha <jsarha@ti.com>
2012-11-08 19:14:17 +0200

    test/testmain.c: Add test case for recoding
    The last test case records from main mic and plays the recording back.
    
    Signed-off-by: Jyri Sarha <jsarha@ti.com>

--- test/testmain.c
+++ test/testmain.c
@@ -13,6 +13,8 @@ extern int ucmhal_adev_open(const hw_module_t* module, const char* name,
 							hw_device_t** device);
 
 void play_sine(struct audio_stream_out *out, size_t frames);
+void read_samples(struct audio_stream_in *in, char *buf, size_t len);
+void write_samples(struct audio_stream_out *out, char *buf, size_t len);
 
 int main(int argc, char *argv[]) {
 	hw_module_t dummy_module = {};
@@ -34,36 +36,43 @@ int main(int argc, char *argv[]) {
 		audio_hw_device_t *adev = (audio_hw_device_t *) dev;
 		audio_io_handle_t handle = 0;
 		audio_output_flags_t flags = 0;
-		struct audio_config config = {
+		struct audio_config config_out = {
 			.sample_rate = 44100,
-			.channel_mask = AUDIO_DEVICE_OUT_ALL,
+			.channel_mask = AUDIO_CHANNEL_OUT_STEREO,
+			.format = AUDIO_FORMAT_PCM_SUB_16_BIT
+		};
+		struct audio_config config_in = {
+			.sample_rate = 48000,
+			.channel_mask = AUDIO_CHANNEL_IN_STEREO,
 			.format = AUDIO_FORMAT_PCM_SUB_16_BIT
 		};
 		struct audio_stream_out *stream_out_hf;
 		struct audio_stream_out *stream_out_hs;
-		char buf[1024];
-		fgets(buf, sizeof(buf), stdin);
+		struct audio_stream_in *stream_in;
+		char getsbuf[1024];
+		fgets(getsbuf, sizeof(getsbuf), stdin);
 
 		if ((ret = adev->open_output_stream(
-				 adev, handle, AUDIO_DEVICE_OUT_SPEAKER, flags, &config, &stream_out_hf))) {
-			fprintf(stderr, "open_input_stream returned %d \n", ret);
+				 adev, handle, AUDIO_DEVICE_OUT_SPEAKER, flags, &config_out, &stream_out_hf))) {
+			fprintf(stderr, "open_output_stream returned %d \n", ret);
 			return -1;
 		}
 
 		play_sine(stream_out_hf, 44100);
-		fgets(buf, sizeof(buf), stdin);
-
 		stream_out_hf->common.standby(&stream_out_hf->common);
 
+		fgets(getsbuf, sizeof(getsbuf), stdin);
+
 		if ((ret = adev->open_output_stream(
-				 adev, handle, AUDIO_DEVICE_OUT_WIRED_HEADSET, flags, &config, &stream_out_hs))) {
-			fprintf(stderr, "open_input_stream returned %d \n", ret);
+				 adev, handle, AUDIO_DEVICE_OUT_WIRED_HEADSET, flags, &config_out, &stream_out_hs))) {
+			fprintf(stderr, "open_output_stream returned %d \n", ret);
 			return -1;
 		}
 
-		fgets(buf, sizeof(buf), stdin);
+		fgets(getsbuf, sizeof(getsbuf), stdin);
 		play_sine(stream_out_hs, 44100);
-		fgets(buf, sizeof(buf), stdin);
+		stream_out_hs->common.standby(&stream_out_hs->common);
+		fgets(getsbuf, sizeof(getsbuf), stdin);
 
 		adev->close_output_stream(adev, stream_out_hs);
 
@@ -76,14 +85,34 @@ int main(int argc, char *argv[]) {
 		stream_out_hf->common.set_parameters(
 			&stream_out_hf->common, AUDIO_PARAMETER_STREAM_ROUTING "=2;");
 		play_sine(stream_out_hf, 44100);
-		fgets(buf, sizeof(buf), stdin);
-		adev->set_parameters(adev, AUDIO_PARAMETER_KEY_SCREEN_STATE "=on;");
-		fgets(buf, sizeof(buf), stdin);
-		adev->set_parameters(adev, AUDIO_PARAMETER_KEY_SCREEN_STATE "=on;");
-		fgets(buf, sizeof(buf), stdin);
-		adev->set_parameters(adev, AUDIO_PARAMETER_KEY_SCREEN_STATE "=off;");
-		fgets(buf, sizeof(buf), stdin);
+		stream_out_hf->common.standby(&stream_out_hf->common);
+		fgets(getsbuf, sizeof(getsbuf), stdin);
 
+		adev->set_parameters(adev, AUDIO_PARAMETER_KEY_SCREEN_STATE "=on;");
+		fgets(getsbuf, sizeof(getsbuf), stdin);
+		adev->set_parameters(adev, AUDIO_PARAMETER_KEY_SCREEN_STATE "=on;");
+		fgets(getsbuf, sizeof(getsbuf), stdin);
+		adev->set_parameters(adev, AUDIO_PARAMETER_KEY_SCREEN_STATE "=off;");
+		fgets(getsbuf, sizeof(getsbuf), stdin);
+
+		if ((ret = adev->open_input_stream(
+				 adev, handle, AUDIO_DEVICE_IN_BUILTIN_MIC, &config_in, &stream_in))) {
+			fprintf(stderr, "open_input_stream returned %d \n", ret);
+			return -1;
+		}
+
+		fgets(getsbuf, sizeof(getsbuf), stdin);
+		char buf[4*96000];
+		read_samples(stream_in, buf, sizeof(buf));
+		stream_in->common.standby(&stream_in->common);
+		fgets(getsbuf, sizeof(getsbuf), stdin);
+		write_samples(stream_out_hf, buf, sizeof(buf));
+		stream_out_hf->common.standby(&stream_out_hf->common);
+		fgets(getsbuf, sizeof(getsbuf), stdin);
+
+		adev->close_input_stream(adev, stream_in);
+
+		fgets(getsbuf, sizeof(getsbuf), stdin);
 		adev->close_output_stream(adev, stream_out_hf);
 
 		dev->close(dev);
@@ -123,3 +152,24 @@ void play_sine(struct audio_stream_out *out, size_t frames) {
 		out->write(out, buf, sizeof(buf));
 	}
 }
+
+void read_samples(struct audio_stream_in *in, char *buf, size_t len) {
+	int i;
+	size_t bufsize = in->common.get_buffer_size(&in->common);
+
+	for (i = 0; i+bufsize < len; i += bufsize) {
+		in->read(in, &buf[i], bufsize);
+	}
+}
+
+
+void write_samples(struct audio_stream_out *out, char *buf, size_t len) {
+	int i;
+	size_t bufsize = out->common.get_buffer_size(&out->common);
+
+	for (i = 0; i+bufsize < len; i += bufsize) {
+		out->write(out, &buf[i], bufsize);
+	}
+}
+
+//void loop_audio(struct audio_stream_out *out, size_t frames) {
